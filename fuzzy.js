132be572353a7d421c4e965d9dcce455c9d421a2
Charlie Groll <charliegroll@gmail.com>
2013-03-21 22:09:52 -0400

    I lied... nowww it's modularized... I hope for real, this time.

--- fuzzy.js
+++ fuzzy.js
@@ -4,160 +4,13 @@
  *
  * Copyright (C) 2013 Charlie Groll, http://charliegroll.com
  */
-var Fuzzy = {};
+function Fuzzy (){
 
-Fuzzy.result = {
-    "1": [],    // starts with
-    "2": [],    // any after starts with
-    "3": [],    // contains
-    "4": []     // has some of (fuzzy)
-};
-
-Fuzzy.indexes = {
-    "1": [],
-    "2": [],
-    "3": [],
-    "4": [] 
-};
-
-// obj: an object to flatten (returns an array, in order, of the object's elements)
-Fuzzy.flatten = function (obj) {
-    if (typeof obj === 'undefined'){
-        return [];
-    } else if (typeof obj !== 'object') {
-        throw "Flatten takes an object as input!";
-    } else if (obj instanceof Array) {
-        return obj;
-    }
-
-    var res = [],
-        k = Object.keys(obj);
-
-    k.forEach(function (el, i) {
-        res = res.concat(obj[el]);
-    });
-
-    return res;
-}
-
-// input: array of what's to be searched
-// query: what we're searching for... duh
-// caseSensitive: boolean, defaults to false (means we don't care about case)
-Fuzzy.search = function (input, query, caseSensitive) {
-    caseSensitive = typeof caseSensitive !== 'undefined' ? caseSensitive : false;
-
-    if (!(input instanceof Array)) {
-        throw "Fuzzy requires an array input!";
-    }
-
-    if (typeof query !== 'string') {
-        throw "Fuzzy requires a string query!";
-    }
-
-    var orig = input.slice(0),
-        o = input.slice(0); // we wanna store the original indexes from input, so we need to keep input intact somewhere
-
-    var add = function (str, place, i) {
-        Fuzzy.result[place].push(str);
-        Fuzzy.indexes[place].push(o.indexOf(str));
-        input.splice(i, 1);
-        orig.splice(i, 1);
-    }
-
-    if (!caseSensitive) {
-        query = query.toLowerCase();
-        for (var i=0; i<input.length; i++) {
-            input[i] = input[i].toLowerCase();
-        }
-    }
-
-    var fuzz = explode(query.toLowerCase(), false);
-    
-    for (var i=0; i<input.length; i++) {
-        var thing = input[i],
-            origthing = orig[i],
-            arr = thing.split(' '),
-            added = false;
-
-
-        if (arr[0].indexOf(query) === 0) {
-            add(origthing, "1", i);
-            added = true;
-        } else {
-            for (var j=0; j<arr.length; j++) {
-                var elem = arr[j];
-                
-                if (elem.indexOf(query) === 0) {
-                    add(origthing, "2", i);
-                    added = true;
-                    break;
-                }
-            }
-
-            if (!added){
-                for (var j=0; j<arr.length; j++) {
-                    var elem = arr[j];
-                    
-                    if (elem.indexOf(query) !== -1) {
-                        add(origthing, "3", i);
-                        added = true;
-                        break;
-                    }
-                }
-            }
-            
-            if (!added){
-                var everything = explode(thing);
-                added = true;
-                for (var j=0; j<fuzz.length; j++) {
-                    if (everything.indexOf(fuzz[j]) === -1) {
-                        added = false;
-                        break;
-                    }
-                }
-
-                if (added) {
-                    add(origthing, "4", i);
-                }
-            }
-        }   
-        if (added) {
-            i--;
-        }
-    }
-    
-    return Fuzzy.result;
-}
-
-// uses Fuzzy.search() with the additional selector param
-// selector: a basic css selector like 'a.class' for which elements in the list should be searched
-Fuzzy.searchElems = function (elems, selector, query, caseSensitive) {
-    var arr = [];
-
-    elems.forEach(function (el, i) {
-        if (isElement(el)) {
-            // find string
-            el.querySelectorAll(selector).forEach(function (el, i) {
-                // no, really... find string.
-            });
-            arr.push()
-        }
-    });
-
-    function isElement(o){
-        return (
-            typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
-                o && typeof o === "object" && o.nodeType === 1 && typeof o.nodeName==="string"
-        );
-    }
-}
-
-Fuzzy.clear = function () {
     this.result = {
-        "1": [],    
-        "2": [],
-        "3": [],
-        "4": []
+        "1": [],    // starts with
+        "2": [],    // any after starts with
+        "3": [],    // contains
+        "4": []     // has some of (fuzzy)
     };
 
     this.indexes = {
@@ -166,7 +19,154 @@ Fuzzy.clear = function () {
         "3": [],
         "4": [] 
     };
-}
+
+    // obj: an object to flatten (returns an array, in order, of the object's elements)
+    this.flatten = function (obj) {
+        if (typeof obj === 'undefined'){
+            return [];
+        } else if (typeof obj !== 'object') {
+            throw "Flatten takes an object as input!";
+        } else if (obj instanceof Array) {
+            return obj;
+        }
+
+        var res = [],
+            k = Object.keys(obj);
+
+        k.forEach(function (el, i) {
+            res = res.concat(obj[el]);
+        });
+
+        return res;
+    }
+
+    // input: array of what's to be searched
+    // query: what we're searching for... duh
+    // caseSensitive: boolean, defaults to false (means we don't care about case)
+    this.search = function (input, query, caseSensitive) {
+        caseSensitive = typeof caseSensitive !== 'undefined' ? caseSensitive : false;
+
+        if (!(input instanceof Array)) {
+            throw "Fuzzy requires an array input!";
+        }
+
+        if (typeof query !== 'string') {
+            throw "Fuzzy requires a string query!";
+        }
+
+        var orig = input.slice(0),
+            o = input.slice(0); // we wanna store the original indexes from input, so we need to keep input intact somewhere
+
+        var add = function (result, indexes, str, place, i) {
+            result[place].push(str);
+            indexes[place].push(o.indexOf(str));
+            input.splice(i, 1);
+            orig.splice(i, 1);
+        }
+
+        if (!caseSensitive) {
+            query = query.toLowerCase();
+            for (var i=0; i<input.length; i++) {
+                input[i] = input[i].toLowerCase();
+            }
+        }
+
+        var fuzz = explode(query.toLowerCase(), false);
+        
+        for (var i=0; i<input.length; i++) {
+            var thing = input[i],
+                origthing = orig[i],
+                arr = thing.split(' '),
+                added = false;
+
+            if (arr[0].indexOf(query) === 0) {
+                add(this.result, this.indexes, origthing, "1", i);
+                added = true;
+            } else {
+                for (var j=0; j<arr.length; j++) {
+                    var elem = arr[j];
+                    
+                    if (elem.indexOf(query) === 0) {
+                        add(this.result, this.indexes, origthing, "2", i);
+                        added = true;
+                        break;
+                    }
+                }
+
+                if (!added){
+                    for (var j=0; j<arr.length; j++) {
+                        var elem = arr[j];
+                        
+                        if (elem.indexOf(query) !== -1) {
+                            add(this.result, this.indexes, origthing, "3", i);
+                            added = true;
+                            break;
+                        }
+                    }
+                }
+                
+                if (!added){
+                    var everything = explode(thing);
+                    added = true;
+                    for (var j=0; j<fuzz.length; j++) {
+                        if (everything.indexOf(fuzz[j]) === -1) {
+                            added = false;
+                            break;
+                        }
+                    }
+
+                    if (added) {
+                        add(this.result, this.indexes, origthing, "4", i);
+                    }
+                }
+            }   
+            if (added) {
+                i--;
+            }
+        }
+        
+        return this.result;
+    }
+
+    // uses Fuzzy.search() with the additional selector param
+    // selector: a basic css selector like 'a.class' for which elements in the list should be searched
+    this.searchElems = function (elems, selector, query, caseSensitive) {
+        var arr = [];
+
+        elems.forEach(function (el, i) {
+            if (isElement(el)) {
+                // find string
+                el.querySelectorAll(selector).forEach(function (el, i) {
+                    // no, really... find string.
+                });
+                arr.push()
+            }
+        });
+
+        function isElement(o){
+            return (
+                typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
+                    o && typeof o === "object" && o.nodeType === 1 && typeof o.nodeName==="string"
+            );
+        }
+    }
+
+    this.clear = function () {
+        this.result = {
+            "1": [],    
+            "2": [],
+            "3": [],
+            "4": []
+        };
+
+        this.indexes = {
+            "1": [],
+            "2": [],
+            "3": [],
+            "4": [] 
+        };
+    }
+};
 
 var explode = function (string, duplicates) {
     var result = [];
@@ -180,6 +180,28 @@ var explode = function (string, duplicates) {
     return result;
 }
 
+// Taken from charliegroll/ECMA262-5
+if (!('indexOf' in Array.prototype)) {
+    Array.prototype.indexOf= function(find, i /*opt*/) {
+        if (i===undefined) i= 0;
+        if (i<0) i+= this.length;
+        if (i<0) i= 0;
+        for (var n= this.length; i<n; i++)
+            if (i in this && this[i]===find)
+                return i;
+        return -1;
+    };
+}
+
+if (!('forEach' in Array.prototype)) {
+    Array.prototype.forEach= function(action, that /*opt*/) {
+        for (var i= 0, n= this.length; i<n; i++)
+            if (i in this)
+                action.call(that, this[i], i, this);
+    };
+}
+
+// let's allow Node usage
 if (typeof exports !== 'undefined') { 
     exports.Fuzzy = Fuzzy;
 }
--- test.js
+++ test.js
@@ -2,7 +2,8 @@ var test = [],
 	result,
 	fs = require('fs'),
 	fuzzy = require('./fuzzy').Fuzzy,
-	fuzz2 = require('./fuzzy').Fuzzy,
+	fuzz = new fuzzy(),
+	fuzz2 = new fuzzy(),
 	filename = '/usr/share/dict/words',
 	limit = process.argv[2] || 100,
 	rl = require('readline').createInterface({
@@ -14,15 +15,16 @@ var test = [],
 	        rl.close();
 	    }
 	}).on('close', function() {
-		fuzzy.clear();
-	    fuzzy.search(test, "dide");
-	    console.log(fuzzy.result);
-	    console.log(fuzzy.flatten(fuzzy.result));
-	    console.log(fuzzy.indexes);
-	    console.log(fuzzy.flatten(fuzzy.indexes));
-	    fuzzy.clear();
-	    console.log(fuzzy.result);
-	    console.log(fuzzy.indexes);
+		fuzz.clear();
+	    fuzz.search(test, "did");
+	    console.log('result:');
+	    console.log(fuzz.result);
+	    console.log('flat result:');
+	    console.log(fuzz.flatten(fuzz.result));
+	    console.log('indexes:');
+	    console.log(fuzz.indexes);
+	    console.log('flat indexes:')
+	    console.log(fuzz.flatten(fuzz.indexes));
 	    console.log();
 	    fuzz2.search(['ed','de','edward','Eddy'], "ed");
 	    console.log(fuzz2.result);
